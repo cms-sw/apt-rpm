744a0ffb430266ee6370ef22c96b948d609304ef
David Abdurachmanov <davidlt@cern.ch>
2013-10-18 17:16:40 +0200

    Refactor support for 10.8, bring back <=10.7 support
    The change to dirent.h was done only in Mac OS X 10.8, for 10.7
    and older releases keep using the old prototype.
    
    Availability.h header is used, thus only 10.6 and 10.7 as old
    targets are supported.
    
    Signed-off-by: David Abdurachmanov <davidlt@cern.ch>

--- tools/genutil.h
+++ tools/genutil.h
@@ -1,3 +1,6 @@
+#if defined(__APPLE__)
+#include <Availability.h>
+#endif
 
 static inline
 bool endswith(const char *str, const char *suffix)
@@ -13,7 +16,11 @@ bool endswith(const char *str, const char *suffix)
 }
 
 static
+#if defined(__FREEBSD__) || ( defined(__APPLE__) && defined(__MAC_OS_X_VERSION_MIN_REQUIRED) &&  __MAC_OS_X_VERSION_MIN_REQUIRED <= 1070)
+int selectRPMs(struct dirent *ent)
+#else
 int selectRPMs(const struct dirent *ent)
+#endif
 {
    const char *d = ent->d_name;
    return (*d != '.' && endswith(d, ".rpm"));
